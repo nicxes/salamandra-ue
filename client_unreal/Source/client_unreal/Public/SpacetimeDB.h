6d927ff1a3f08efd5dd0b785fd429010c55495f6
Nicxes <nicxesh@gmail.com>
2025-10-05 16:46:31 -0300

    push(init): Initial commit

---
+++ client_unreal/Source/client_unreal/Public/SpacetimeDB.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "SpacetimeDB.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CLIENT_UNREAL_API USpacetimeDB : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+};
